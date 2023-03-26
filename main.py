d374d159ac0859492cff6efcc42d4d246a9f3d32
nxtperfect <alakaxan@protonmail.com>
2023-03-26 21:20:12 +0200

    preparing for actual commits, adding libraries and initial classes

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-import pyautogui
+import pyautogui # pyautogui.move() to move the mouse to new position
+import pynput # Create Listener object with pynput.keyboard
 import PyQt5
+import decimal
+
+class Settings():
+    mouse_dpi = 1000
+    in_game_dpi = decimal.Decimal('1.0')
+    resolution = (1920, 1080)
+    degrees_to_rotate = 360
+
+class Gui():
+    pass
+
+class Mouse_movement():
+    pass
 
 if __name__ == "__main__":
     run()
