63af3bdb8f1fcfe16b3aab285c1e053e58667583
nxtperfect <alakaxan@protonmail.com>
2023-04-01 20:06:46 +0200

    Implemented basic function that moves the mouse right and down by your screen resolution divided by 10

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pyautogui # pyautogui.move() to move the mouse to new position
-import pynput # Create Listener object with pynput.keyboard
+from pynput import keyboard # Create Listener object with pynput.keyboard
 import PyQt5
 import decimal, math
 
@@ -10,7 +10,7 @@ class Settings:
     resolution = (1920, 1080)
     degrees_to_rotate = 360
     fov = 90.0
-    # keybind = kb_F5 
+    keybind = keyboard.Key.f6 
 
     def __init__(self, mouse_dpi: int = 1000, in_game_sensitivity: float = 1.0, system_sensitivity: float = 1.0, resolution: tuple[int, int] = [1920, 1080], degrees_to_rotate: int = 360, fov: float = 90.0):
         decimal.getcontext().prec = 6
@@ -21,22 +21,44 @@ class Settings:
         self.degrees_to_rotate = degrees_to_rotate
         self.fov = fov
 
-class Gui():
+class Inputs:
+    def on_press(self, key = keyboard.Key.f6, pos_x: float = 100, pos_y: float = 100):
+        try:
+            if key == keyboard.Key.f6:
+                print('F6 key was pressed')
+                Mouse_movement.move_mouse(pos_x, pos_y)
+        except AttributeError:
+            pass
+
+class Gui:
     pass
 
-class Mouse_movement():
+class Mouse_movement:
     def pixels_per_degree(self, mouse_dpi: int = 4000, system_sensitivity: float = 1.0, in_game_sensitivity: float = 1.0, resolution: tuple[int, int] = [1920, 1080], fov: float = 90.0) -> float:
         return round(resolution[1] / 2) / math.degrees(math.tan(fov/2))
+
+    def move_mouse(self, pos_x: float = 0, pos_y: float = 0):
+        pyautogui.moveRel(pos_x, pos_y)
 
 def run():
     # First we create settings class in order to save all the necessary info
     settings = Settings()
-    # Here we ask the user for input on their mouse dpi, in game dpi, degrees to rotate, and read their resolution
+    # Here we ask the user for input on their in game dpi, degrees to rotate, and read their resolution
 
     # Calculate how much do we need to move the mouse based on dpi's, resolution and how many degrees to de want to turn
     test = Mouse_movement()
     print(f'for {settings.mouse_dpi} dpi, {settings.in_game_sensitivity} in game sens, {settings.system_sensitivity} system sens, you need {test.pixels_per_degree()} pixels of movement to turn your camera by 1 degree')
-    pass
+
+    # Create input to make action on pressed key
+    input = Inputs()
+
+    # Create listener that will run input.on_press function whenever a key is pressed
+    listener = keyboard.Listener(on_press=input.on_press)
+    listener.start()
+    listener.join()
+
+    while 1:
+        input.on_press(keyboard.Key.f6)
 
 if __name__ == "__main__":
     run()
