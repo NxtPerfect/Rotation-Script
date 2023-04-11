06d714e0fd1f16d8370c1c728c98b213997ff6f3
nxtperfect <alakaxan@protonmail.com>
2023-04-11 19:21:55 +0200

    Added possibility to change direction, the script now moves the mouse on x axis, currently the gui doesn't open because of keyboard listener, will be fixed in the next commit

--- main.py
+++ main.py
@@ -3,33 +3,46 @@ from pynput import keyboard # Create Listener object with pynput.keyboard
 import tkinter as tk # GUI
 from tkinter import ttk # GUI widgets
 import decimal, math # Floating point accuracy
+from typing import List
 
 # Class used to change main settings of the program, GUI interacts with it by scrollbars and input fields
 class Settings:
-    mouse_dpi = 1000
-    in_game_sensitivity = decimal.Decimal('1.0')
-    system_sensitivity = decimal.Decimal('1.0')
-    resolution = (1920, 1080)
+    in_game_sensitivity = 1.0
+    resolution = [1920, 1080]
     degrees_to_rotate = 360
+    duration = 1.0
     fov = 90.0
-    duration = decimal.Decimal('1.0')
-    keybind = keyboard.Key.f6 
+    keybind = keyboard.Key.f6
+    direction_left = False 
+    pyautogui.FAILSAFE = False 
 
-    def __init__(self, mouse_dpi: int = 1000, in_game_sensitivity: float = 1.0, system_sensitivity: float = 1.0, resolution: tuple[int, int] = [1920, 1080], degrees_to_rotate: int = 360, fov: float = 90.0):
-        decimal.getcontext().prec = 6
-        self.mouse_dpi = mouse_dpi
-        self.in_game_sensitivity = decimal.Decimal(in_game_sensitivity)
-        self.system_sensitivity = decimal.Decimal(system_sensitivity)
+    def __init__(self, in_game_sensitivity: float = 1.0, resolution: List[int] = [1920, 1080], degrees_to_rotate: int = 360, fov: float = 90.0, direction_left : bool = True):
+        self.in_game_sensitivity = in_game_sensitivity
         self.resolution = resolution
         self.degrees_to_rotate = degrees_to_rotate
         self.fov = fov
+        self.direction_left = direction_left
+
+# Class that is used to move the mouse as well as calculate how many pixels we want to turn the mouse for
+class Mouse_movement:
+    @staticmethod
+    def pixels_per_degree(in_game_sensitivity: float = 1.0, resolution: List[int] = [1920, 1080], fov: float = 90.0):
+        return float(round(resolution[1] / 2.0) / math.degrees(math.tan(fov/2.0)))
+
+    @staticmethod
+    def move_mouse(pos_x: int = 0, pos_y: int = 0):
+        pyautogui.moveRel(pos_x, pos_y, Settings.duration)
 
 class Inputs:
-    def on_press(self, key = keyboard.Key.f6, pos_x: int = pyautogui.size()[0]/10, pos_y: int = pyautogui.size()[1]/10):
+    @staticmethod
+    def on_press(self, key: keyboard = keyboard.Key.f6, pos_x: int = (Mouse_movement.pixels_per_degree(Settings.in_game_sensitivity, Settings.resolution, Settings.fov)*Settings.degrees_to_rotate), pos_y: int = 0):
+        if Settings.direction_left:
+            pos_x = -pos_x
         try:
-            if key == keybind:
+            if key == Settings.keybind:
                 print(f'{Settings.keybind.name} key was pressed')
                 print(f'Before move {pyautogui.position()}')
+                print(f'{pos_x}')
                 Mouse_movement.move_mouse(pos_x, pos_y)
                 print(f'After move {pyautogui.position()}')
         except AttributeError:
@@ -45,16 +58,11 @@ class Gui:
 
     def __init__(self, pos_x: int=0, pos_y: int=0):
         self.script_label = ttk.Label(master = self.window, text = 'Rotation Script', font = 'Ubuntu 18')
-        self.fov_label = ttk.Label(master = self.window, text = f'FOV: {Settings.fov}')
-        self.sensitivity_label = ttk.Label(master = self.window, text = f'Sensitivtiy: {Settings.in_game_sensitivity}')
+        self.fov_label = ttk.Label(master = self.window, text = f'Fov: {Settings.fov}')
+        self.sensitivity_label = ttk.Label(master = self.window, text = f'Sensitivity: {Settings.in_game_sensitivity}')
         self.duration_label = ttk.Label(master = self.window, text = f'Duration time: {Settings.duration}')
         self.keybind_label = ttk.Label(master = self.window, text = f'Keybind: {Settings.keybind}')
-        self.script_label = ttk.Label(master = self.window, text = 'Rotation Script', font = 'Ubuntu 18')
-
-        self.fov_label = ttk.Label(master = self.window, text = f'FOV: {Settings.fov}')
-        self.sensitivity_label = ttk.Label(master = self.window, text = f'Sensitivtiy: {Settings.in_game_sensitivity}')
-        self.duration_label = ttk.Label(master = self.window, text = f'Duration time: {Settings.duration}')
-        self.keybind_label = ttk.Label(master = self.window, text = f'Keybind: {Settings.keybind}')
+        self.direction_label = ttk.Label(master = self.window, text = f'Direction (left = true, right = false): {Settings.direction_left}')
 
         self.window.title('xxx')
         #self.window.geometry('200x150')
@@ -62,7 +70,8 @@ class Gui:
         self.fov_spinbox = ttk.Spinbox(master = self.window, from_ = decimal.Decimal('10.0'), to = decimal.Decimal('180.0'), increment = 0.1, command = self.update_fov, validatecommand = (self.window.register(self.float_validate)))
         self.sensitivity_spinbox = ttk.Spinbox(master = self.window, from_ = decimal.Decimal('0.01'), to = decimal.Decimal('10.0'), increment = 0.01, command = self.update_sensitivity, validate = 'all', validatecommand = (self.window.register(self.float_validate)))
         self.duration_spinbox = ttk.Spinbox(master = self.window, from_ = decimal.Decimal('0.1'), to = decimal.Decimal('60.0'), increment = 0.1 , command = self.update_duration, validate = 'all', validatecommand = (self.window.register(self.float_validate)))
-        self.keybind_button = ttk.Button(master = self.window, text = 'Change keybind', command = self.change_keybind) # TODO: Needs to listen to keybind after that instead of trying to immediately change it
+        self.keybind_button = ttk.Button(master = self.window, text = 'Change keybind', command = self.update_keybind) # TODO: Needs to listen to keybind after that instead of trying to immediately change it
+        self.direction_button = ttk.Button(master = self.window, text = 'Change direction', command = self.update_direction)
 
         self.script_label.grid(row = 0, column = 0, columnspan = 2)
 
@@ -70,12 +79,14 @@ class Gui:
         self.sensitivity_spinbox.grid(row = 2, column = 0)
         self.duration_spinbox.grid(row = 3, column = 0)
         self.keybind_button.grid(row = 4, column = 0)
+        self.direction_button.grid(row = 5, column = 0)
 
         # Draw labels onto the grid
         self.fov_label.grid(row = 1, column = 1)
         self.sensitivity_label.grid(row = 2, column = 1)
         self.duration_label.grid(row = 3, column = 1)
         self.keybind_label.grid(row = 4, column = 1)
+        self.direction_label.grid(row = 5, column = 1)
 
     def update_fov(self):
         Settings.fov = decimal.Decimal(self.fov_spinbox.get())
@@ -88,6 +99,12 @@ class Gui:
     def update_duration(self):
         Settings.duration = decimal.Decimal(self.duration_spinbox.get())
         self.duration_label['text'] = f'Duration time: {Settings.duration}s'
+
+    def update_keybind(key):
+        Settings.keybind = key
+
+    def update_direction(self):
+        Settings.direction_left = not Settings.direction_left
 
     # If shift, control or alt is pressed, read the next input and add that to the keybind
     def change_keybind(self, key):
@@ -107,13 +124,7 @@ class Gui:
             return False
 
 
-# Class that is used to move the mouse as well as calculate how many pixels we want to turn the mouse for
-class Mouse_movement:
-    def pixels_per_degree(self, mouse_dpi: int = 4000, system_sensitivity: float = 1.0, in_game_sensitivity: float = 1.0, resolution: tuple[int, int] = [1920, 1080], fov: float = 90.0) -> float:
-        return round(resolution[1] / 2) / math.degrees(math.tan(fov/2))
 
-    def move_mouse(pos_x: int = 0, pos_y: int = 0):
-        pyautogui.moveRel(pos_x, pos_y, 2)
 
 def run():
     # First we create settings class in order to save all the necessary info
@@ -122,21 +133,22 @@ def run():
 
     # Calculate how much do we need to move the mouse based on dpi's, resolution and how many degrees to de want to turn
     test = Mouse_movement()
-    print(f'for {settings.mouse_dpi} dpi, {settings.in_game_sensitivity} in game sens, {settings.system_sensitivity} system sens, you need {test.pixels_per_degree()} pixels of movement to turn your camera by 1 degree')
+    print(f'for {settings.in_game_sensitivity} in game sens, you need {test.pixels_per_degree()} pixels of movement to turn your camera by 1 degree')
 
     # Create input to make action on pressed key
     input = Inputs()
 
     # Create listener that will run input.on_press function whenever a key is pressed
-    #listener = keyboard.Listener(on_press=input.on_press)
-    #listener.start()
-    #listener.join()
+    listener = keyboard.Listener(on_press=input.on_press)
+    listener.start()
+    listener.join()
 
     gui = Gui()
-    gui.window.mainloop()
+    #gui.window.mainloop()
 
-    #while 1:
-    #    input.on_press(keyboard.Key.f6)
+    while 1:
+        input.on_press(Settings.keybind)
+        gui.window.mainloop()
 
 if __name__ == "__main__":
     run()
